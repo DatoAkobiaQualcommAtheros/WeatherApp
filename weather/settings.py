c3b39b81778bf8b4817a29646a1c4f92f78a427f
QualcommAtheros <davidakobia3@gmail.com>
2019-04-20 17:08:12 +0400

    Add background image

--- weather/settings.py
+++ weather/settings.py
@@ -108,7 +108,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
